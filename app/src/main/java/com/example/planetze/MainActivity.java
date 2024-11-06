2db3a26f12136c7411dbe2c5d2804c78278a818d
OkIsOkk <aahmad01000001@gmail.com>
2024-11-06 13:05:45 -0500

    Addtional Refactoring

--- app/src/main/java/com/example/planetze/MainActivity.java
+++ app/src/main/java/com/example/planetze/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.planetze;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -21,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         db = FirebaseDatabase.getInstance("https://b07-demo-summer-2024-default-rtdb.firebaseio.com/");
         DatabaseReference myRef = db.getReference("testDemo");
 
-//      myRef.setValue("B07 Demo!");
+//        myRef.setValue("B07 Demo!");
         myRef.child("movies").setValue("B07 Demo!");
 
         if (savedInstanceState == null) {
