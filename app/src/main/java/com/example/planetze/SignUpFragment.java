0d5a108f0d1d7483abe6cd156286b270849bd671
Minki Jiang <minkijiang@Minkis-MacBook-Pro.local>
2024-11-09 10:39:29 -0500

    modified google-services.json

--- app/src/main/java/com/example/planetze/SignUpFragment.java
+++ app/src/main/java/com/example/planetze/SignUpFragment.java
@@ -1,12 +1,24 @@
 package com.example.planetze;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,12 +27,13 @@ import android.view.ViewGroup;
  */
 public class SignUpFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword,signupConfirmPassword;
+    private Button signupButton;
+    private TextView loginRedirectText;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +41,6 @@ public class SignUpFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SignUpFragment newInstance(String param1, String param2) {
         SignUpFragment fragment = new SignUpFragment();
         Bundle args = new Bundle();
@@ -53,12 +57,74 @@ public class SignUpFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = view.findViewById(R.id.edit_email);
+        signupPassword = view.findViewById(R.id.edit_password);
+        signupConfirmPassword = view.findViewById(R.id.edit_confirmpassword);
+        signupButton = view.findViewById(R.id.createaccount_button);
+        loginRedirectText = view.findViewById(R.id.loginRedirectText);
+
+        Activity activity = getActivity();
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+
+                // Set up Validation Logic
+                if (email.isEmpty()) {
+                    signupEmail.setError("Email cannot be empty");
+                } else if (pass.isEmpty()) {
+                    signupPassword.setError("Password cannot be empty");
+                } else {
+                    auth.createUserWithEmailAndPassword(email, pass)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        auth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(activity, "Signup Successful, please check email for verification.", Toast.LENGTH_LONG).show();
+                                                    startActivity(new Intent(activity, MainActivity.class));
+                                                    // Toast.makeText(this, "please check email for verification.", Toast.LENGTH_SHORT).show();
+                                                    //loadingBar.dismiss();
+                                                }else{
+                                                    Toast.makeText(activity, task.getException().getMessage() , Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+                                        });
+
+                                    } else {
+                                        Toast.makeText(activity, "Signup Failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(activity, MainActivity.class));
+            }
+        });
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+
     }
 }
