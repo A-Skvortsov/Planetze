1f74871718c2a7db7edc3e4dd15ad5a195afed18
Andrey Skvortsov <andreyskv49@gmail.com>
2024-11-12 11:06:45 -0500

    modified: SurveyResults.java, Constants.java
    added the countries and their correspoding emissions to constants file, made the spinner interactive and loaded with the required countries, UI now (partially) updates with changes to spinner

--- app/src/main/java/com/example/planetze/SurveyResults.java
+++ app/src/main/java/com/example/planetze/SurveyResults.java
@@ -6,6 +6,9 @@ import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,7 +19,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import utilities.Constants;
+
 public class SurveyResults extends AppCompatActivity {
+
+    final String[] country = Constants.country;
+    final double[] country_emissions = Constants.country_emissions;
+    String default_country = Constants.default_country;
+    double user_e = 0.0;  //total user emissions
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +43,33 @@ public class SurveyResults extends AppCompatActivity {
         double[] results = intent.getDoubleArrayExtra("co2PerCategory");
         results = kgToTons(results);
         setCategoryGraph(results);
+
+        user_e = sum(results);
+        initComparisonGraph(default_country);
+
+        //spinner change listener
+        Spinner s = findViewById(R.id.spinner);
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                setComparisonGraph(selectedItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                setComparisonGraph(default_country);
+            }
+        });
+
     }
 
+
+    /**
+     * Sets the first graph depicting breakdown of user emissions by category
+     * @param results
+     */
     public void setCategoryGraph(double[] results) {
         TextView[] bars = {
                     findViewById(R.id.transport),
@@ -52,11 +87,14 @@ public class SurveyResults extends AppCompatActivity {
         //230 is based on container width/bar width calculations in XML
         //230 = 358 (container width) - 104 (x value of start of bar) - 24 (extra spacing)
         // Converts container width to pixels
-        int container_width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 230, getResources().getDisplayMetrics());
+        int container_width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                230, getResources().getDisplayMetrics());
         DisplayMetrics display_metrics = getResources().getDisplayMetrics();
         // Convert dp to pixels. 35dp chosen based on UI
-        int min_p = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 25, display_metrics);
+        int min_p = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                25, display_metrics);
 
+        //implementing bar length (updating ui)
         for (int i = 0; i < bars.length; i++) {
             //computes pixel width for current bar
             int p = Math.max(min_p, (int) (container_width * (results[i] / max_result)));
@@ -71,6 +109,57 @@ public class SurveyResults extends AppCompatActivity {
             bars[i].setText(txt);
         }
     }
+
+
+    /**
+     * Initializes second graph (comparison of user emissions with a selected country)
+     * to default country and loads spinner with list of countries
+     * @param c the default country
+     */
+    private void initComparisonGraph(String c) {
+        //initialize spinner to country c
+        Spinner s = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                 android.R.layout.simple_spinner_item, country);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+        int init = adapter.getPosition(c);
+        s.setSelection(init);
+
+        setComparisonGraph(c);
+    }
+
+
+    /**
+     * Updates the comparison graph based on current spinner setting
+     * @param c the country selected by spinner for which we display average emissions
+     */
+    private void setComparisonGraph(String c) {
+        int i = indexOf(country, c);
+        double country_e = country_emissions[indexOf(country, c)];
+
+        //computes desired bar height of compared country
+        double height_user_in_p = findViewById(R.id.user_bar).getHeight();
+        // Convert dp to pixels. 25dp and 125dp chosen based on UI
+        DisplayMetrics display_metrics = getResources().getDisplayMetrics();
+        int min_p = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                25, display_metrics);
+        int max_p = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                125, display_metrics);
+        //next line is main formula
+        int height_in_p =
+                (int) Math.min(max_p, Math.max(min_p, (country_e/user_e) * height_user_in_p));
+
+        //sets bar height of compared country
+        TextView bar = findViewById(R.id.country_bar);
+        bar.setHeight(height_in_p);
+        TextView bar_height = findViewById(R.id.country_emissions);
+
+        //sets text of bar height textbox
+        String x = String.valueOf(round(country_e)) + "   ";
+        bar_height.setText(x);
+    }
+
 
     /**
      * Converts array of carbon emissions in kg to tons
@@ -105,6 +194,23 @@ public class SurveyResults extends AppCompatActivity {
         x = x * 10;
         int y = (int) x;
         return y / 10.0;
+    }
+
+    /**
+     * Self-explanatory. Java has no built-in array.indexOf function,
+     * need one to convert spinner selection to country emissions
+     * @param arr
+     * @param item
+     * @return
+     */
+    private int indexOf(String[] arr, String item) {
+        int x = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(item)) {
+                x = i; break;
+            }
+        }
+        return x;
     }
 
 }
