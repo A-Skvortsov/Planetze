d4d888715c3dab7b43d9b5f90ebbdbdf79a0ce48
OkIsOkk <aahmad01000001@gmail.com>
2024-11-18 17:21:26 -0500

    Replaced the Survey and SurveyResult activities with fragments.

--- app/src/main/java/com/example/planetze/SurveyResults.java
+++ app/src/main/java/com/example/planetze/SurveyResults.java
@@ -1,9 +1,9 @@
 package com.example.planetze;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -11,17 +11,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import utilities.Constants;
 
-public class SurveyResults extends AppCompatActivity {
+public class SurveyResults extends Fragment {
 
     final String[] country = Constants.country;
     final double[] country_emissions = Constants.country_emissions;
@@ -29,49 +26,54 @@ public class SurveyResults extends AppCompatActivity {
     double user_e = 0.0;  //total user emissions
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_survey_results);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_survey_results, container, false);
+        ViewCompat.setOnApplyWindowInsetsListener(view.findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Intent intent = getIntent();
-        double[] results = intent.getDoubleArrayExtra("co2PerCategory");
+        Bundle args = getArguments();
+
+        assert args != null;
+        double[] results = args.getDoubleArray("co2PerCategory");
+
+        assert results != null;
         results = kgToTons(results);
         user_e = sum(results);  //saves user result immediately
 
         //initializes some basics
-        final TextView your_emissions = findViewById(R.id.your_emissions);
+        final TextView your_emissions = view.findViewById(R.id.your_emissions);
             String sum = round(user_e) + "   ";
             your_emissions.setText(sum);
-        final TextView total_bar = findViewById(R.id.total_bar);
+        final TextView total_bar = view.findViewById(R.id.total_bar);
             String msg = sum + " tons of CO2 emitted annually";
             total_bar.setText(msg);
-        setCategoryGraph(results);  //sets category graph
+        setCategoryGraph(view, results);  //sets category graph
 
-        initComparisonGraph(default_country);  //initializes comparison graph
-        setUserDataComparisonGraph(user_e);
+        initComparisonGraph(view, default_country);  //initializes comparison graph
+        setUserDataComparisonGraph(view, user_e);
 
         //spinner change listener
-        Spinner s = findViewById(R.id.spinner);
+        Spinner s = view.findViewById(R.id.spinner);
         s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view,
+            public void onItemSelected(AdapterView<?> parent, View v,
                                        int position, long id) {
                 String selectedItem = (String) parent.getItemAtPosition(position);
-                setComparisonGraph(selectedItem);
+
+                // Use a current view
+                setComparisonGraph(view, selectedItem);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                setComparisonGraph(default_country);
+                setComparisonGraph(view, default_country);
             }
         });
 
+        return view;
     }
 
 
@@ -79,17 +81,17 @@ public class SurveyResults extends AppCompatActivity {
      * Sets the first graph depicting breakdown of user emissions by category
      * @param results
      */
-    public void setCategoryGraph(double[] results) {
+    public void setCategoryGraph(View view, double[] results) {
         TextView[] bars = {
-                    findViewById(R.id.transport),
-                    findViewById(R.id.food),
-                    findViewById(R.id.housing),
-                    findViewById(R.id.consumption)};
+                    view.findViewById(R.id.transport),
+                    view.findViewById(R.id.food),
+                    view.findViewById(R.id.housing),
+                    view.findViewById(R.id.consumption)};
         TextView[] extra = {
-                    findViewById(R.id.t_negligible),
-                    findViewById(R.id.f_negligible),
-                    findViewById(R.id.h_negligible),
-                    findViewById(R.id.c_negligible)};
+                    view.findViewById(R.id.t_negligible),
+                    view.findViewById(R.id.f_negligible),
+                    view.findViewById(R.id.h_negligible),
+                    view.findViewById(R.id.c_negligible)};
         double max_result = max(results);
 
         //preprocessing
@@ -125,17 +127,17 @@ public class SurveyResults extends AppCompatActivity {
      * to default country and loads spinner with list of countries
      * @param c the default country
      */
-    private void initComparisonGraph(String c) {
+    private void initComparisonGraph(View view, String c) {
         //initialize spinner to country c
-        Spinner s = findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+        Spinner s = view.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(),
                  android.R.layout.simple_spinner_item, country);  //used to load spinner
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);  //loads spinner
         int init = adapter.getPosition(c);  //initializes spinner to default country
         s.setSelection(init);  //^^
 
-        setComparisonGraph(c);  //set UI
+        setComparisonGraph(view, c);  //set UI
     }
 
 
@@ -143,12 +145,14 @@ public class SurveyResults extends AppCompatActivity {
      * Updates the comparison graph based on current spinner setting
      * @param c the country selected by spinner for which we display average emissions
      */
-    private void setComparisonGraph(String c) {
+    private void setComparisonGraph(View view, String c) {
         int i = indexOf(country, c);
         double country_e = country_emissions[indexOf(country, c)];
 
+        assert view.findViewById(R.id.user_bar) != null;
+
         //computes desired bar height of compared country
-        double height_user_in_p = findViewById(R.id.user_bar).getHeight();
+        double height_user_in_p = view.findViewById(R.id.user_bar).getHeight();
         // Convert dp to pixels. 13dp and 125dp chosen based on UI
         DisplayMetrics display_metrics = getResources().getDisplayMetrics();
         int min_p = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
@@ -160,14 +164,14 @@ public class SurveyResults extends AppCompatActivity {
                 (int) Math.min(max_p, Math.max(min_p, Math.abs((country_e/user_e) * height_user_in_p)));
 
         //sets bar height of compared country
-        TextView bar = findViewById(R.id.country_bar);
+        TextView bar = view.findViewById(R.id.country_bar);
         ViewGroup.LayoutParams params = bar.getLayoutParams();
         params.height = height_in_p;
         bar.setLayoutParams(params);
         //bar.requestLayout();  //possibly necessary. For now no issues without this
 
         //sets text of bar height textbox
-        TextView bar_height = findViewById(R.id.country_emissions);
+        TextView bar_height = view.findViewById(R.id.country_emissions);
         String x = String.valueOf(round(country_e)) + "   ";
         bar_height.setText(x);
     }
@@ -179,8 +183,8 @@ public class SurveyResults extends AppCompatActivity {
      * Otherwise default setting of user bar is about half the size of the container
      * @param e user emissions
      */
-    private void setUserDataComparisonGraph(double e) {
-        TextView user_bar = findViewById(R.id.user_bar);
+    private void setUserDataComparisonGraph(View view, double e) {
+        TextView user_bar = view.findViewById(R.id.user_bar);
         ViewGroup.LayoutParams params = user_bar.getLayoutParams();
         DisplayMetrics display_metrics = getResources().getDisplayMetrics();
         int height_in_p = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
