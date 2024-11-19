aa63728a4118b8c977c5b6ea0650d6bfba05ef1c
Andrey Skvortsov <andreyskv49@gmail.com>
2024-11-19 11:19:34 -0500

    modified: EcoTracker both files
    testing loading the activities section with activities that can be selected for editing/deletion

--- app/src/main/java/com/example/planetze/EcoTrackerActivity.java
+++ app/src/main/java/com/example/planetze/EcoTrackerActivity.java
@@ -3,6 +3,8 @@ package com.example.planetze;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
@@ -23,6 +31,10 @@ import utilities.Constants;
 public class EcoTrackerActivity extends AppCompatActivity {
 
     final String[] months = Constants.months;
+    private FirebaseAuth auth;
+    private FirebaseDatabase db;
+    private DatabaseReference userRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +47,21 @@ public class EcoTrackerActivity extends AppCompatActivity {
             return insets;
         });
 
+        db = FirebaseDatabase.getInstance("https://planetze-c3c95-default-rtdb.firebaseio.com/");
+        auth = FirebaseAuth.getInstance();
+        userRef = db.getReference("user data")
+                .child("IHdNxXO2pGXsicTlymf5HQAaUnL2")
+                .child("test_data");
+
         final Button calendarToggle = findViewById(R.id.calendarToggle);  //button to toggle calendar
         final ConstraintLayout calendarView = findViewById(R.id.calendarView);  //view containing calendar
         final MaterialCalendarView calendar = findViewById(R.id.calendar);  //calendar itself
         final TextView dateText = findViewById(R.id.dateText);
         final TextView yearText = findViewById(R.id.yearText);
 
-        final Button activities = findViewById(R.id.activities);
-        final Button habits = findViewById(R.id.habits);
+        final Button activitiesBtn = findViewById(R.id.activitiesBtn);
+        final RadioGroup activities = findViewById(R.id.activitiesGroup);
+        final Button habitsBtn = findViewById(R.id.habitsBtn);
 
         //initializing to current date
         Calendar today = Calendar.getInstance();
@@ -61,11 +80,12 @@ public class EcoTrackerActivity extends AppCompatActivity {
                     calendarToggle.setText("View Calendar");
                 } else {
                     calendarView.setVisibility(View.VISIBLE);
-                    calendarToggle.setText("Select Day");
+                    calendarToggle.setText("Select");
                 }
 
             }
         });
+
 
         //Changes presented information when new date selected
         calendar.setOnDateChangedListener(new OnDateSelectedListener() {
@@ -76,12 +96,14 @@ public class EcoTrackerActivity extends AppCompatActivity {
                 String date = String.valueOf(d) + " " + months[m-1];
                 dateText.setText(date);
                 yearText.setText(String.valueOf(day.getYear()));
+
+                updateDisplay(activities);
             }
         });
 
 
         //show user activities
-        activities.setOnClickListener(new View.OnClickListener() {
+        activitiesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //should clear the linearlayout of any views and display activity data as stored
@@ -89,13 +111,25 @@ public class EcoTrackerActivity extends AppCompatActivity {
             }
         });
         //show user habits
-        habits.setOnClickListener(new View.OnClickListener() {
+        habitsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
 
-
     }
+
+    final private String[] testArr = {"activity 1", "activity 2", "activity 3"};
+    public void updateDisplay(RadioGroup activities) {
+        activities.clearCheck(); activities.removeAllViews();
+        RadioButton btn;
+        for (int i = 0; i < testArr.length; i++) {
+            btn = new RadioButton(EcoTrackerActivity.this);
+            btn.setId(i);
+            btn.setText(testArr[i]);
+            activities.addView(btn);
+        }
+    }
+
 }
