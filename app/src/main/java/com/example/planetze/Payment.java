049074fbad2b931bf4baf0823f8f48dfa9cee511
Nethanel Reta <nethanel.n4@gmail.com>
2024-11-22 16:50:20 -0500

    Skeleton for StringRequest along with documentation

--- app/src/main/java/com/example/planetze/Payment.java
+++ app/src/main/java/com/example/planetze/Payment.java
@@ -2,27 +2,26 @@ package com.example.planetze;
 
 import android.os.Bundle;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
 import android.widget.Button;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.planetze.databinding.ActivityPaymentBinding;
 import com.stripe.android.PaymentConfiguration;
 import com.stripe.android.paymentsheet.PaymentSheet;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Payment extends AppCompatActivity {
 
@@ -30,9 +29,11 @@ public class Payment extends AppCompatActivity {
     private ActivityPaymentBinding binding;
     Button btn;
     String PUSHABLE_KEY = "pk_test_51QNyLBEClbgNHzIwl1LvxmmIIbAJfT0jkrXaxorbPqbtTVGeqhKlEoeBNNNXltdSENhs9sVE2PNdR5GMowGXNB1j00WkAP3Xzi";
+    String SECRET_KEY; // ur not allowed to see this
 
     PaymentSheet paymentSheet;
     StringRequest stringRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,25 +50,56 @@ public class Payment extends AppCompatActivity {
 
         });
 
+        String url = "cus_RGW7sv4XRGFzAT";
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST,
-                "https://api.stripe.com/v1/customers  ",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+        // sets up a systsem to get networks reuqets
+        RequestQueue requestQueue =  Volley.newRequestQueue(this);
 
-                    }
-                }, new Response.ErrorListener() {
+
+        //sends a request and expects a string response , to in this case stripe
+        //request.Method.POST specfices the type of request u want to make, this case send data (POST)
+        // url the server where ur sending ur info to
+        //the 2 functions handles a requets that was succesful, and one that was failed ie a error
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+
+            new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // will implement later
+
+            }
+
+        },
+            new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
-            }
-        });
+                // will implement later
 
-        RequestQueue requestQueue = Volley.newRequestQueue(Payment.this);
-        requestQueue.add(stringRequest);
+            }
+
+        }){
+
+
+            // provides addition info which may be needed when sending our request to the sever
+            // in this case the authorizatio which tells stripe were allowed to use their application through our given api key
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map header = new HashMap();
+                // provides proof that u are allowed to make this request to our servie ie STRIPE
+                header.put("Authorization", "Bearer "+SECRET_KEY);
+                return header;
+            }
+        };
+
+
+
+
 
 
     }
+
+
+
 
 }
