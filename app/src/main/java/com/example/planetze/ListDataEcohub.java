a57167db05b3493c91432b6f7ef79d52e6fcb824
Nethanel Reta <nethanel.n4@gmail.com>
2024-11-27 12:48:44 -0500

    Added ListData for EcoHub

--- app/src/main/java/com/example/planetze/ListDataEcohub.java
+++ app/src/main/java/com/example/planetze/ListDataEcohub.java
@@ -3,24 +3,18 @@ package com.example.planetze;
 public class ListDataEcohub {
     int images;
     String description;
-    String location;
 
-    public ListDataEcohub(int img, String des, String loc){
+    public ListDataEcohub(int img, String des){
         this.images = img;
         this.description = des;
-        this.location = loc;
     }
 
     public int getImages() {
         return images;
     }
 
-    public String getLocation() {
-        return location;
-    }
 
     public String getDescription() {
         return description;
     }
-}
 }
