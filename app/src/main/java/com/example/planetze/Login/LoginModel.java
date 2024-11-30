7ce1f0d32cc0f16b3524b702d60ce6563e67835e
Ahmad <106856979+OkIsOkk@users.noreply.github.com>
2024-11-30 03:27:35 -0500

    Bugfix/aqeeb-ahmad/UI-bugs (#31)
    - Improved the settings page UI
    - Added additional settings to the settings page
    - Fixed UI issues with the survey where the category textview and buttons would be hidden by the CardView on smaller screens
    - Fixed an issue where the log-in and sign-up screen banner wouldn't expand on larger screens
    - Fixed an Eco Gauge bug where the comparison chart would show the incorrect percentage
    - Fixed the splash screen logo

--- app/src/main/java/com/example/planetze/Login/LoginModel.java
+++ app/src/main/java/com/example/planetze/Login/LoginModel.java
@@ -17,7 +17,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -68,7 +67,7 @@ public class LoginModel {
 
     private void takeToHomePage(LoginPresenter presenter) {
 
-        if (UserData.is_new_user(presenter.getViewContext())) {
+        if (UserData.isNewUser(presenter.getViewContext())) {
             presenter.takeToSurvey();
         }
         else {
--- app/src/main/java/com/example/planetze/SettingActivity.java
+++ app/src/main/java/com/example/planetze/SettingActivity.java
@@ -4,23 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.planetze.Login.LoginView;
-import com.google.android.material.snackbar.Snackbar;
 
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.planetze.databinding.ActivitySettingBinding;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -29,11 +21,10 @@ import utilities.UserData;
 
 public class SettingActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivitySettingBinding binding;
-
     private SwitchMaterial stayLoggedOn;
     private SwitchMaterial interpolateEmissionsData;
+    private SwitchMaterial hideGridLinesSwitch;
+    private SwitchMaterial showTrendLinePointsSwitch;
     private Button returnButton;
     private Button logoutButton;
 
@@ -45,61 +36,78 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        stayLoggedOn = findViewById(R.id.stay_logged_on_switch);
         returnButton = findViewById(R.id.returnButton);
         logoutButton = findViewById(R.id.logoutButton);
+        stayLoggedOn = findViewById(R.id.stay_logged_in_switch);
         interpolateEmissionsData = findViewById(R.id.ied_switch);
+        showTrendLinePointsSwitch = findViewById(R.id.show_trend_line_points_switch);
+        hideGridLinesSwitch = findViewById(R.id.hide_grid_lines_switch);
 
         initialize();
 
-        returnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent j = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(j);
+        returnButton.setOnClickListener(view -> {
+            Intent j = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(j);
 
+        });
+
+        logoutButton.setOnClickListener(view -> {
+            UserData.logout(getApplicationContext());
+            loadFragment(new LoginView());
+        });
+
+        stayLoggedOn.setOnClickListener(view -> {
+            String userID = UserData.getUserID(getApplicationContext());
+
+            db = FirebaseDatabase.getInstance("https://planetze-c3c95-default-rtdb.firebaseio.com/");
+            userRef = db.getReference("user data");
+            if (stayLoggedOn.isChecked()) {
+                userRef.child(userID+"/settings/stay_logged_on").setValue(true);
+            }
+            else {
+                userRef.child(userID+"/settings/stay_logged_on").setValue(false);
+            }
+
+        });
+
+        interpolateEmissionsData.setOnClickListener(view -> {
+            String userID = UserData.getUserID(getApplicationContext());
+
+            db = FirebaseDatabase.getInstance("https://planetze-c3c95-default-rtdb.firebaseio.com/");
+            userRef = db.getReference("user data");
+            if (interpolateEmissionsData.isChecked()) {
+                userRef.child(userID+"/settings/interpolate_emissions_data").setValue(true);
+            }
+            else {
+                userRef.child(userID+"/settings/interpolate_emissions_data").setValue(false);
+            }
+
+        });
+
+        hideGridLinesSwitch.setOnClickListener(view -> {
+            String userID = UserData.getUserID(getApplicationContext());
+
+            db = FirebaseDatabase.getInstance("https://planetze-c3c95-default-rtdb.firebaseio.com/");
+            userRef = db.getReference("user data");
+            if (hideGridLinesSwitch.isChecked()) {
+                userRef.child(userID+"/settings/hide_grid_lines").setValue(true);
+            }
+            else {
+                userRef.child(userID+"/settings/hide_grid_lines").setValue(false);
             }
         });
 
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                UserData.logout(getApplicationContext());
-                loadFragment(new LoginView());
+
+        showTrendLinePointsSwitch.setOnClickListener(view -> {
+            String userID = UserData.getUserID(getApplicationContext());
+
+            db = FirebaseDatabase.getInstance("https://planetze-c3c95-default-rtdb.firebaseio.com/");
+            userRef = db.getReference("user data");
+            if (showTrendLinePointsSwitch.isChecked()) {
+                userRef.child(userID+"/settings/show_trend_line_points").setValue(true);
             }
-        });
-
-        interpolateEmissionsData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String userID = UserData.getUserID(getApplicationContext());
-
-                db = FirebaseDatabase.getInstance("https://planetze-c3c95-default-rtdb.firebaseio.com/");
-                userRef = db.getReference("user data");
-                if (stayLoggedOn.isChecked()) {
-                    userRef.child(userID+"/settings/interpolate_emissions_data").setValue(true);
-                }
-                else {
-                    userRef.child(userID+"/settings/interpolate_emissions_data").setValue(false);
-                }
-
-            }
-        });
-
-        stayLoggedOn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String userID = UserData.getUserID(getApplicationContext());
-
-                db = FirebaseDatabase.getInstance("https://planetze-c3c95-default-rtdb.firebaseio.com/");
-                userRef = db.getReference("user data");
-                if (stayLoggedOn.isChecked()) {
-                    userRef.child(userID+"/settings/stay_logged_on").setValue(true);
-                }
-                else {
-                    userRef.child(userID+"/settings/stay_logged_on").setValue(false);
-                }
-
+            else {
+                userRef.child(userID+"/settings/show_trend_line_points").setValue(false);
             }
         });
     }
